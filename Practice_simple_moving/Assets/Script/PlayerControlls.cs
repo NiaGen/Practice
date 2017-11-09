bbeabf6832f814bbc5376bf7571086005c1a1162
MaximusWells <max.wells.sn@outlook.com>
2017-11-09 21:20:25 +0200

    Extremely simple 2d movement
    Extremely simple 2d movement.

---
+++ Practice_simple_moving/Assets/Script/PlayerControlls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlls : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		float moveHor = Input.GetAxis ("Horizontal");
+		player.velocity = new Vector2 (moveHor * speed, player.velocity.y);
+	}
+}
