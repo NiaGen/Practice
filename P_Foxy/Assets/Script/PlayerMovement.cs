1fd95d87392c40edeb432a7a075e76b106d11ce9
MaximusWells <max.wells.sn@outlook.com>
2017-11-10 05:37:36 +0200

    Platformer game
    Begining of practice on the little game.

---
+++ P_Foxy/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D player;
+	public float speed = 1f;
+	bool directionR = true;
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		if (move != 0.0f) {
+			animator.SetFloat("Player_run", 1.0f);
+		} else {
+			animator.SetFloat("Player_run", 0.0f);
+		}
+//		animator.SetFloat("Player_run", move);
+		player.velocity = new Vector2 (move * speed, player.velocity.y);
+
+		if (move > 0 && !directionR)
+		{
+			Flip ();
+		}
+		else if (move < 0 && directionR)
+		{
+			Flip ();
+		}
+	}
+
+	void Flip()
+	{
+		directionR = !directionR;
+		Vector2 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
